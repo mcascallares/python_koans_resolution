98a642b3d541af561fde408b2e757657cf0f150b
Matias Cascallares <matiascas@gmail.com>
2011-11-14 10:57:32 +0100

    walking the path of enlightment

--- python 2/koans/triangle.py
+++ python 2/koans/triangle.py
@@ -17,6 +17,7 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
+    validate(a, b, c)
     if a == b and b == c:
         return 'equilateral'
     elif a != b and b != c and c != a:
@@ -24,6 +25,9 @@ def triangle(a, b, c):
     else:
         return 'isosceles'
 
-# Error class used in part 2.  No need to change this code.
+def validate(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0: raise TriangleError
+    if a >= b + c or b >= a + c or c >= a + b: raise TriangleError    
+
 class TriangleError(StandardError):
     pass
